66b9468b23edb19ac97c388be0a793e82af934cf
Rohith Subramanyam <rohith.subramanyam@nutanix.com>
2018-08-23 04:52:29 -0700

    Adding debug logs to uninstall

--- installer/install.py
+++ installer/install.py
@@ -73,8 +73,10 @@ def run_cmd(cmd, err=None):
 
 def restore_file(file_path):
   """Restore the latest backup."""
+  LOGGER.DEBUG("Restoring %s", file_path)
   bkps = sorted(glob.glob("%s*.bkp" % file_path))
   if bkps:
+    LOGGER.debug("backups: %s", bkps)
     shutil.move(os.path.join(bkps[-1]), file_path)
 
 def uninstall():
